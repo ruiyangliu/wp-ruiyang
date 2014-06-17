db34b32500a1e4d276c7d61c88ebcc57132c03f6
richard <richard@ruiyang.(none)>
2014-06-16 23:06:31 -0400

    some changes

--- wp-content/themes/ruiyang_theme/header.php
+++ wp-content/themes/ruiyang_theme/header.php
@@ -5,6 +5,7 @@
 <html>
   <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, user-scalable=no">
     <title>Richard Ruiyang Liu</title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" />
     <link rel="stylesheet" href="<?php print($template_url.'/stylesheets/application.css') ?>" />
--- wp-content/themes/ruiyang_theme/index.php
+++ wp-content/themes/ruiyang_theme/index.php
@@ -2,7 +2,7 @@
 
 <div id="content" class="content-bg">
   <?php while(have_posts()): the_post() ?>
-    <h2><?php the_title() ?></h2>
+    <h2><a href="<?php echo get_permalink(); ?>"><?php the_title() ?></a></h2>
     <?php the_content(__('Continue Reading')); ?>
   <?php endwhile; ?>
 </div>
